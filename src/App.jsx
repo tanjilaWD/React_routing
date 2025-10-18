05ae4e25570fad18048112f8de96ebcec55cc76b
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-18 09:26:12 +0600

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import Services from './Components/Service';
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
